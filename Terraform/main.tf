381354b415400f4c48be4ff57c6d7a94ae04f044
loki <loki1711@hotmail.com>
2022-11-22 23:55:03 +0530

    TF destroy

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,11 +3,3 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
   tags     = var.tag
 }
-
-resource "azurerm_virtual_network" "vnet" {
-name                = "github-action-vnet"
-address_space       = ["10.0.0.0/16"]
-location            = azurerm_resource_group.resource_group.location
-resource_group_name = azurerm_resource_group.resource_group.name
-tags                = var.tag
-}
